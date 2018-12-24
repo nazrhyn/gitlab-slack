0e0c657d44a297cfe9127e52d55c36e84d087bed
chris <genjuroa@ymail.com>
2018-12-25 00:23:32 +0800

    property define updated

--- app/handlers/pipeline.js
+++ app/handlers/pipeline.js
@@ -14,28 +14,28 @@ const debug = debugCreate('gitlab-slack:handler:pipeline');
 module.exports = function (data) {
 	debug('Handling message...');
 
-    const attachment = {
-        mrkdwn_in: ['text'],
-        color: module.exports.COLOR,
-        title: data.commit.message,
-        title_link: data.commit.url
-    };
+	const attachment = {
+		mrkdwn_in: ['text'],
+		color: module.exports.COLOR,
+		title: data.commit.message,
+		title_link: data.commit.url
+	};
 
 	const wikiDetails = data.object_attributes,
 		output = {
 			parse: 'none',
 			text: util.format(
 				'[%s] <%s/pipelines/%s|pipeline %s> by <%s/u/%s|%s> %s',
-                data.project.name,
+				data.project.name,
 				data.project.web_url,
-                wikiDetails.id,
-                wikiDetails.id,
+				wikiDetails.id,
+				wikiDetails.id,
 				data.project.web_url,
 				data.user.username,
-                data.user.username,
-                wikiDetails.status
-            ),
-            attachments: [attachment]
+				data.user.username,
+				wikiDetails.status
+			),
+			attachments: [attachment]
 		};
 
 	debug('Message handled.');
@@ -45,20 +45,20 @@ module.exports = function (data) {
 	return Promise.resolve(output);
 };
 
-Object.defineProperty(
-    module.exports,
-    {
-        'KIND':
+Object.defineProperties(
+	module.exports,
+	{
+		KIND:
         {
-            enumerable: true,
-            value: Object.freeze({
-                name: 'pipeline',
-                title: 'pipeline'
-            })
+         	enumerable: true,
+        	value: Object.freeze({
+        		name: 'pipeline',
+        		title: 'pipeline'
+        	})
         },
-        'COLOR': {
-            enumerable: true,
-            value: '#31B93D'
-        }
-    }
+		COLOR: {
+			enumerable: true,
+			value: '#31B93D'
+		}
+	}
 );
